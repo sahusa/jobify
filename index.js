4974291a3c52fc96e87de2719a8ef20058d3a43b
sahusa <sahusa.vinicius@gmail.com>
2019-05-08 21:48:47 -0300

    Adicionado port

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser')
 const sqlite = require('sqlite')
 const dbConnection = sqlite.open('banco.sqlite', { Promise})
 
+const port = process.env.PORT || 3000
+
 app.set('view engine', 'ejs')// com isso estamos indicando que usaremos o ejs como linguagem de template
 app.use(express.static('public'))//Diretorio alternativo para o express localizar arquivos que não estão no "get /"
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -97,7 +99,7 @@ const init = async() => {
     //await db.run(`insert into vagas(categoria, titulo, descricao) values(2, '${vaga}', '${descricao}')`)
 }
 init()
-app.listen(3000, (err) => {
+app.listen(port, (err) => {
     if(err){console.log('Não foi possível iniciar o servidor do Jobify')}else{
         console.log('Servidor do Jobify rodando...')
     }
